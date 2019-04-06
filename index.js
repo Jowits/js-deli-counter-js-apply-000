38ac43a9a269e12db417df09d8b46b2f630ad7a7
Jowita Lapies <jowita.lapies@gmail.com>
2019-04-06 15:17:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,10 +2,7 @@ katzDeli = [];
 
 function takeANumber(line, name){
   line.push(name);
-
-  console.log("Welcome, " + name + ". You are number " + line.length + " in line.");
-
-  return "Welcome, " + name + ". You are number " + line.length + " in line."
+  return "Welcome,  ${name}. You are number ${line.length} in line."
 
 }
 
